7718d5e18d527c0964ca42b969c8d98574f65446
Meng Wang <justwangmeng@outlook.com>
2026-02-07 19:44:08 +0800

    删除 midai-main，只保留 midai 项目

--- midai-main/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'albums.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
--- midai-main/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.routes import init_app
-
-app = create_app()
-init_app(app)
-
-if __name__ == "__main__":
-    print("Template search path:", app.jinja_loader.searchpath)  # 打印模板搜索路径
-    app.run(debug=True)
