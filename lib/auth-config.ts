fa6440fcf3fd4939b639a167a725c8193e546fb6
Meng Wang <justwangmeng@outlook.com>
2026-02-13 21:06:13 +0800

    fix(auth): fix sameSite type error

--- lib/auth-config.ts
+++ lib/auth-config.ts
@@ -68,7 +68,7 @@ export const authConfig = {
         : 'next-auth.session-token',
       options: {
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'lax' as const,
         path: '/',
         secure: process.env.NODE_ENV === 'production',
       },
